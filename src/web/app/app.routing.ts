ec97def73f24ed94eb11d939a410438ca3a72fea
Joshua Wiens <joshuaw@easymetrics.com>
2016-10-29 03:11:33 -0500

    feat(app): add base route

---
+++ src/web/app/app.routing.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+import { FeaturesComponent } from './features/features.component';
+
+export const routes: Routes = [
+  { path: '', component: FeaturesComponent, pathMatch: 'full' }
+];
