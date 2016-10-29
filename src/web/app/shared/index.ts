c36deb6849366c2b52e3d89aa1db6c9872cfa54f
Joshua Wiens <joshuaw@easymetrics.com>
2016-10-29 03:04:42 -0500

    feat(shared): shared web application components

---
+++ src/web/app/shared/index.ts
@@ -0,0 +1,82 @@
+/**
+ * Shared Module
+ * Allows sharing of Components, Services & Vendor modules
+ * across the application.
+ */
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+
+import {
+
+} from './components';
+
+import {
+
+} from './directives';
+
+import {
+
+} from './pipes';
+
+import {
+
+} from './services';
+
+import {
+
+} from './validators';
+
+const ANGULAR2_MODULES = [
+    CommonModule,
+    HttpModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule
+];
+
+const ASU_COMPONENTS = [
+
+];
+
+const ASU_DIRECTIVES = [
+
+];
+
+const ASU_PIPES = [
+
+];
+
+const ASU_SERVICES = [
+
+];
+
+const ASU_VALIDATORS = [
+
+];
+
+@NgModule({
+  declarations: [
+    ...ASU_DIRECTIVES,
+    ...ASU_COMPONENTS,
+    ...ASU_PIPES
+  ],
+  imports: [
+    ...ANGULAR2_MODULES
+  ],
+  providers: [
+    RouterModule,
+    ...ASU_SERVICES
+  ],
+  exports: [
+    ...ANGULAR2_MODULES,
+    ...ASU_DIRECTIVES,
+    ...ASU_COMPONENTS,
+    ...ASU_PIPES
+  ]
+})
+export class SharedModule {
+}
